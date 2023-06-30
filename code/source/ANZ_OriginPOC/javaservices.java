0a39e085f565530b03ff37c6695eb2edea7908f2
rahuldemocenter <rahs@softwareag.com>
2023-06-30 12:04:32 +0530

    first commit.

---
+++ code/source/ANZ_OriginPOC/javaservices.java
@@ -0,0 +1,38 @@
+package ANZ_OriginPOC;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class javaservices
+
+{
+	// ---( internal utility methods )---
+
+	final static javaservices _instance = new javaservices();
+
+	static javaservices _newInstance() { return new javaservices(); }
+
+	static javaservices _cast(Object o) { return (javaservices)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void addTwoNos (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(addTwoNos)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
